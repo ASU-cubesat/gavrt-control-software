63ffe6659365d29a08ff9df193193cac17f1da2f
Lindsay Berkhout <lmberkhout@gmail.com>
2024-01-22 17:50:36 -0700

    adding freq set functionality

--- src/utils.py
+++ src/utils.py
@@ -17,12 +17,15 @@ def live_plot_ivq():
          data = parse_packet()
          x = []
          y = []
+         angle = []
          #plt.clf()
          for i in range(8200//2):
              x += [data[2*i]]
              y += [data[2*i + 1]]
+             angle += np.arctan(float(data[2*i])/float(data[2*i+1]))
+         plt.plot(angle)
          plt.clf()
-         plt.plot(x,y)
+         #plt.plot(x,y)
          plt.pause(0.01)
     plt.show()
     return()
@@ -38,8 +41,10 @@ def live_plot_i():
             x += [data[2*i]]
             y += [data[2*i + 1]]
         plt.clf()
-        plt.plot(np.abs(np.fft.fft((x))))
-        plt.xlim(0,8200//4)
+        freq = np.linspace(0,200,8200//2)
+        plt.plot(freq, np.abs(np.fft.fft((x))))
+        #x = np.linspace(0,200,8200//2) 
+        #plt.xlim(0,8200//4)
         plt.pause(0.01)
     plt.show()
     return()
@@ -54,34 +59,39 @@ def live_plot_q():
             x += [data[2*i]]
             y += [data[2*i + 1]]
         plt.clf()
-        plt.plot(np.abs(np.fft.fft((y))))
-        plt.xlim(0,8200//4)
+        freq = np.linspace(0,200,8200//2) 
+        plt.plot(freq, np.abs(np.fft.fft((y))))
+        #plt.xlim(0,8200//4)
         plt.pause(0.01)
     plt.show()
     return()
 
-def live_plot_iq(lofreq,dlfreq,bw=False):
-    plt.figure()
+def live_plot_iq(cfreq,vmax):
+    spectrum = np.zeros_like((10000,4200))
+    fig,axs = plt.subplots(2)
+    packet_cnt = 0 
     while 1:
         data = parse_packet()
         iq = []
-        #x = []
-        #y = []
-        #iq = data[::2] + 1j*data[1::2]
-        #x = [0]*8200//2
-        #offset = lofreq-dlfreq
-        #dlbb = 250-offset
-        x = np.linspace(lofreq-250,lofreq-50,8200//2)
+        x = np.linspace(cfreq-100,cfreq+100,4100)
         for i in range(8200//2):
             iq.append(data[2*i] + 1j*data[2*i + 1])
-        plt.clf()
-        plt.plot(x,np.abs(np.fft.fft((iq))))
-        if bw is not False:
-            plt.xlim(dlfreq-bw/2,dlfreq+bw/2)
+        #plt.clf()
+        axs[0].clear()
+        axs[1].clear()
+        fftd=np.fft.fftshift(np.abs(np.fft.fft(iq)))
+        if packet_cnt < 10000:    
+            pass
         else:
-            plt.xlim(lofreq-250,lofreq-50)
-        plt.xlabel("Frequency(MHz)")
+            pass
+        axs[0].plot(x,fftd)
+        axs[0].set_xlabel("Frequency(MHz)")
+        axs[0].set_title("Live IQ Spectrum")
+        axs[1].imshow(spectrum, vmax=vmax)
+        axs[1].set_title('IQ Waterfall')
+        packet_cnt+=1
         plt.pause(0.01)
+
     plt.show()
     return()
 
