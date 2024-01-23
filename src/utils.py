f7c3a842182fb392b2b8940d1fdd8efaf545a994
Lindsay Berkhout <lmberkhout@gmail.com>
2024-01-22 20:47:09 -0700

    fixes

--- src/utils.py
+++ src/utils.py
@@ -66,31 +66,55 @@ def live_plot_q():
     plt.show()
     return()
 
-def live_plot_iq(cfreq,vmax):
-    spectrum = np.zeros_like((10000,4200))
-    fig,axs = plt.subplots(2)
+def live_plot_iq(cfreq,vmin=100,vmax=10000):
+    spectrum = []
+    fig,axs = plt.subplots(2,2)
+    plt.tight_layout()
     packet_cnt = 0 
     while 1:
         data = parse_packet()
         iq = []
+        ilist= []
+        qlist = []
         x = np.linspace(cfreq-100,cfreq+100,4100)
+        x2 = np.linspace(cfreq, cfreq+100,2051)
         for i in range(8200//2):
             iq.append(data[2*i] + 1j*data[2*i + 1])
+            ilist.append(data[2*i+1])
+            qlist.append(data[2*i])
+        
         #plt.clf()
-        axs[0].clear()
-        axs[1].clear()
+        axs[0,0].clear()
+        axs[0,1].clear()
+        axs[1,0].clear()
+        axs[1,1].clear()
+
         fftd=np.fft.fftshift(np.abs(np.fft.fft(iq)))
-        if packet_cnt < 10000:    
-            pass
+        fftd.reshape(4100)
+
+        if packet_cnt < 100:    
+            spectrum.append(fftd) 
         else:
-            pass
-        axs[0].plot(x,fftd)
-        axs[0].set_xlabel("Frequency(MHz)")
-        axs[0].set_title("Live IQ Spectrum")
-        axs[1].imshow(spectrum, vmax=vmax)
-        axs[1].set_title('IQ Waterfall')
+            spectrum.pop(0)
+            spectrum.append(fftd)
+
+        axs[0,0].plot(x,fftd)
+        axs[0,0].set_xlabel("Frequency(MHz)")
+        axs[0,0].set_title("Live IQ Spectrum")
+        
+        axs[1,0].plot(x2, np.abs(np.fft.rfft(ilist)))
+        axs[1,0].set_xlabel("Frequency(MHz)")
+        axs[1,0].set_title("Live I Spectrum")
+
+        axs[1,1].plot(x2, np.abs(np.fft.rfft(qlist)))
+        axs[1,1].set_xlabel("Frequency(MHz)")
+        axs[1,1].set_title("Live Q Spectrum")
+
+        axs[0,1].imshow(spectrum,aspect='auto',vmin=100,vmax=1000)
+        axs[0,1].set_title('IQ Waterfall')
+        
         packet_cnt+=1
-        plt.pause(0.01)
+        plt.pause(0.001)
 
     plt.show()
     return()
